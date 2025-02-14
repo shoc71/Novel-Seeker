447292b9a10b4feab6f0804b1d2d2d95d66ca3fe
shoc71 <supersonicwind6@gmail.com>
2025-02-13 19:02:56 -0500

    connection to db attempt

---
+++ server/src/config/server.db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+// making connection for mongoose to connect mongoDB
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.error("Error: ", error)
+        process.exit(1) // process code; 0 - success, 1 - failure
+    }
+}
