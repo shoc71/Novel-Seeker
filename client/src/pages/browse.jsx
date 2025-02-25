0b4283475a3e3dbd226c6b3927de89ed7b4749a1
courtney ponder <courtney@rednop.com>
2025-02-24 21:31:37 -0500

    updated searchbar

--- client/src/pages/browse.jsx
+++ client/src/pages/browse.jsx
@@ -1,39 +1,46 @@
-import react, {useState} from "react";
-
+import React, { useState } from "react";
+import SearchCard from "../components/SearchCard"; 
 import axios from "axios";
 
+
 const BrowsePage = () => {
-    const [search,setSearch]=useState("");
-    const [bookData,setData]=useState([]);
-    const searchBook=(evt)=>{
-        if(evt.key==="Enter"){
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyDsAloQeKEJ0dFzk_2q9SRkG7Knmp_0Pak' + '&maxResults=40')
-            .then(res=>setData(res.data.items))
-            .catch(err=>console.log(err))
+    const [search, setSearch] = useState("");
+    const [bookData, setData] = useState([]);
+
+    const searchBook = (evt) => {
+        if (evt.key === "Enter" || evt.type === "click") { 
+            axios.get('https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=AIzaSyDsAloQeKEJ0dFzk_2q9SRkG7Knmp_0Pak' + '&maxResults=40')
+                .then(res => setData(res.data.items))
+                .catch(err => console.log(err));
         }
-    }
-    return(
+    };
+
+    return (
         <>
             <div className="header">
-            <div className="row1">
-                <h1>Novel Seeker</h1>
-            </div>
-            <div className="row2">
-                <h2>Search for a book:</h2>
-                <div className="search">
-                    <input type="text" placeholder="Enter the book name here" 
-                    value={search} onChange={e=>setSearch(e.target.value)}
-                    onkeyPress={searchBook} />
-                    <button><i class="fas fa-search"></i></button>
+                <div className="row1">
+                    <h1>Novel Seeker</h1>
+                </div>
+                <div className="row2">
+                    <h2>Search for a book:</h2>
+                    <div className="search">
+                        <input
+                            type="text"
+                            placeholder="Enter the book name here"
+                            value={search}
+                            onChange={e => setSearch(e.target.value)}
+                            onKeyDown={searchBook}
+                        />
+                        <button onClick={searchBook}>Search</button>
+                    </div>
                 </div>
             </div>
 
-        </div><div className="container">
-            {
-            <searchCard book={bookData}/>
-            }
+            <div className="container">
+                <SearchCard book={bookData} /> 
             </div>
-            </>
-    )
-}
+        </>
+    );
+};
+
 export default BrowsePage;
