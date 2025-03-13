6556bbdef64178737ff397e87f2f4999ea4e4368
courtney ponder <courtney@rednop.com>
2025-03-13 15:13:21 -0400

    updated browse

--- client/src/pages/BrowsePage.jsx
+++ client/src/pages/BrowsePage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Modal from "./Modal";
+import Modal from "./modal";
 import axios from "axios";
 import SearchCard from "./SearchCard"; 
 import { Link } from 'react-router-dom';
